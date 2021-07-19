13252ac2e33bc3f776916a375e95616ae895c277
Altynai-Bai <harmoni5413@gmail.com>
2021-07-19 15:20:44 -0500

    added some awesome code

--- src/work1/HelloWorld.java
+++ src/work1/HelloWorld.java
@@ -3,6 +3,7 @@ package work1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Adding awesome codes here");
 
     }
 }
