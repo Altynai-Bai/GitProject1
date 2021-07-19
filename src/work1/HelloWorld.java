6d1a803ee1f2ef2e97cc881046065ef777cf3c58
Altynai-Bai <harmoni5413@gmail.com>
2021-07-19 15:18:46 -0500

    initial commit

--- src/work1/HelloWorld.java
+++ src/work1/HelloWorld.java
@@ -2,7 +2,7 @@ package work1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello git world");
+        System.out.println("Hello git");
 
     }
 }
